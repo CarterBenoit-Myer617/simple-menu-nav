56cc3685705d47c6964d6ff6a932e7c7045a5ade
CarterBenoit-Myer617 <81767200+CarterBenoit-Myer617@users.noreply.github.com>
2022-02-10 08:59:12 -0500

    up

--- dev/js/burgerAnimation.js
+++ dev/js/burgerAnimation.js
@@ -5,15 +5,15 @@ gsap.registerPlugin(DrawSVGPlugin);
 gsap.set(".burger-lines",{transformOrigin:"center"});
 gsap.set("#outline",{drawSVG: "0%"});
 
-const topTL = new gsap.timeline();
-topTL.to(".burger-lines:nth-child(1)",{duration:0.25, y:"+=8"})
-.to(".burger-lines:nth-child(1)",{duration:0.25, rotation:45,fill:"#fff"});
+const topTL = new gsap.timeline(burgerTL);
+topTL.to(".burger-lines:nth-child(1)",{duration:0.25, y:"-=30"})
+.to(".burger-lines:nth-child(1)",{duration:0.25, rotation:45, fill:"#fff"});
 
-const bottomTL = new gsap.timeline();
-bottomTL.to(".burger-lines:nth-child(3)",{duration:0.25, y:"-=8"})
+const bottomTL = new gsap.timeline(burgerTL);
+bottomTL.to(".burger-lines:nth-child(3)",{duration:0.25, y:"+=30"})
 .to(".burger-lines:nth-child(3)",{duration:0.25, rotation:-45,fill:"#fff"});
 
-const middleTL = new gsap.timeline();
+const middleTL = new gsap.timeline(burgerTL);
 middleTL.to(".burger-lines:nth-child(2)",{duration:0.25, scale:0})
 .to("#outline",{duration:0.25, drawSVG:"100%", stroke:"#f91f1f"});
 
@@ -22,3 +22,4 @@ export const burgerTL = new gsap.timeline({paused:true});
 burgerTL.add(topTL,"burger")
     .add(bottomTL,"burger")
     .add(middleTL,"burger")
+;
--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,74 +1,61 @@
-mport { burgerTL } from "./burgerAnimation";
-import {menuAnimation} from "./mobileMenu.js"
+import {
+    burgerTL
+} from "./burgerAnimation";
+// import {menuAnimation} from "./mobileMenu.js"
 
-import { displayWindowSize } from "./mobileResizing"
+// import { displayWindowSize } from "./mobileResizing"
 
 var burgerButton = document.querySelector("#burger");
-let canISeeMenu = false;
 
-function openCloseMenu(){
-    if(canISeeMenu === false){
-        // can't see menu... play timeline of burger into X
+
+burgerButton.addEventListener("click", () => {
+
+
+    burgerTL.play();
+
+
+
+    if (canISeeMenu === false) {
+        
         burgerTL.play();
-        menuAnimation.play();
+        // menuAnimation.play();
         canISeeMenu = true;
-    }else{
-        // can see menu.. play X back into burger
+    } else {
+        
         burgerTL.reverse();
-        menuAnimation.reverse();
-        canISeeMenu = false;
+        // menuAnimation.reverse();
+        canISeeMenu = true;
+    }
+
+
+});
+
+
+
+
+
+burgerButton.addEventListener("click", openCloseMenu);
+let navButtons = document.querySelectorAll(".nav-btns");
+
+for (const button of navButtons){
+    button.addEventListener("click", checkScrolling);
+    button.addEventListener("click", openCloseMenu);
+}
+
+function checkScrolling(e) {
+    // check to see which button was clicked
+    const indexValue = [].indexOf.call(navButtons, e.target)
+    if (indexValue != -1) {
+        scrollPage(indexValue);
     }
 }
 
-// burgerButton.addEventListener("click", openCloseMenu);
-// let navButtons = document.querySelectorAll(".nav-btns");
-
-// for (const button of navButtons){
-//     button.addEventListener("click", checkScrolling);
-//     button.addEventListener("click", openCloseMenu);
-// }
-
-// function checkScrolling(e) {
-//     // check to see which button was clicked
-//     const indexValue = [].indexOf.call(navButtons, e.target)
-//     if (indexValue != -1) {
-//         scrollPage(indexValue);
-//     }
-// }
-
 window.addEventListener('load', function(){
 
-    // // // index values               0                   1                   2
-    // let triggerElements = ["#history-middle","#history-first", "#skills-content"];
-    // // index values                  0                   1                   2
-    // let animationElements =["#history-middle p","#robot-profile", "#skills-content h1"];
-    // let buttonIndex = 0;
 
-    // for( const element of triggerElements){
-    //     // console.log(buttonIndex);
-    //     // console.log(element + " is an element");
-        
-    //     if(element === "#history-first"){
-    //         // console.log("The index is " + buttonIndex);
-    //     }
-    //     buttonIndex ++;
-    // }
 
-    // for(let i = 0; i < triggerElements.length; i++){
-    //     // console.log(triggerElements[i]);
-    //     if(triggerElements[i] === "#skills-content" || triggerElements[i] === "#history-middle"){
-    //         // console.log(i + " is the index value of triggerElements");
-    //     }
-    //     if( i === triggerElements.length/2){
-    //         slideInLeft(triggerElements[i], animationElements[i]);
-    //     }else{
-    //         slideInRight(triggerElements[i], animationElements[i]);
-    //     }
-    // }
-});
-
-//listen to window resize
+// //listen to window resize
 window.addEventListener("resize", displayWindowSize);
 
-//check the window size on load
+// //check the window size on load
 window.addEventListener('load', displayWindowSize); 
